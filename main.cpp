49c91236a5a6eefb00ea5d271f945c36e3770f00
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-10-21 16:32:38 -0500

    Creacion de main y de funcion modificar

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void modificar(int *a){
+    *a = *a + 1;
+}
+
+int main()
+{
+    /*int *b = nullptr;
+    b = new int[100];
+    cout << b << " " << *b << endl;
+    delete[] b;*/
+    /*int a=12;
+    cout << a << endl;
+    cout << &a << endl;
+    modificar(&a);
+    cout << a << endl;*/
+
+    return 0;
+}
